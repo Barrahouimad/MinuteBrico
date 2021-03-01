5eb888a11df898bb70213b020ce9913b41c5a422
imad barrahou <imadbarrahou99@gmail.com>
2021-03-01 02:45:08 +0100

    user and bricoleur

--- confusion/src/Components/HomeComponent.js
+++ confusion/src/Components/HomeComponent.js
@@ -5,6 +5,7 @@ import {Modal, ModalHeader,ModalBody,ModalFooter,Form} from 'reactstrap';
 function Head(){
  const [keyconnection,setkeyc]=useState(false);
  const [keysignup,setkeys]=useState(false);
+ const [isBrico,setBrico]=useState(false);
  const [fullname,setFullname]=useState("");
  const [email,setemail]=useState("");
  const [password,setpassword]=useState("");
@@ -66,6 +67,103 @@ function handlchangeemail(event){
           credentials :"same-origin"
  });
   }
+  const inscrire=(isBrico,keysignup,toggles)=>{
+    if(isBrico){
+      return(
+        <Modal isOpen={keysignup} toggle={toggles}>
+        <ModalHeader toggle={toggles}>Inscrivez vous</ModalHeader>
+        <ModalBody>
+            <Form>
+            <div class="mb-3 row">
+                  <label for="nom" className="col-sm-2 col-form-label">Nom complet</label>
+                  <div className="col-sm-10">
+                  <input type="text" onChange={handlchangename} readonly className="form-control-plaintext" id="nom"/>
+                  </div>
+           </div>
+           <div className="mb-3 row">
+                  <label for="domaine" className="col-sm-2 col-form-label">Domaine</label>
+                  <div className="col-sm-10">
+                  <input type="text" onChange={handlchangedomaine} readonly className="form-control-plaintext" id="domaine" placeholder="Votre domaine ou spécialité"/>
+                  </div>
+           </div>
+           <div className="mb-3 row">
+           <label for="exampleDataList" className="col-2 form-label">Ville</label>
+              <input className="col-10 form-control" onChange={handlchangeville}list="datalistOptions" id="exampleDataList" placeholder="votre ville"/>
+              <datalist id="datalistOptions">
+              <option value="Tanger"/>
+              <option value="Tetouan"/>
+              <option value="Chefchaouen"/>
+              <option value="Hoceima"/>
+              <option value="Asila"/>
+              </datalist>
+           </div>
+            <div className="mb-3 row">
+                  <label for="staticEmail1" className="col-sm-2 col-form-label">Email</label>
+                  <div className="col-sm-10">
+                  <input type="text" readonly onChange={handlchangeemail} className="form-control-plaintext" id="staticEmail1" placeholder="Votre Email"/>
+                  </div>
+           </div>
+          <div className="mb-3 row">
+                  <label for="inputPassword" className="col-sm-2 col-form-label">Mot de passe</label>
+                  <div className="col-sm-10">
+                  <input type="password" onChange={handlchangepass} className="form-control" id="inputPassword"/>
+                  </div>
+          </div>
+          <div className="mb-3">
+          <label for="formFileMultiple" className="form-label">Photo et CV</label>
+          <input className="form-control" type="file" id="formFileMultiple" multiple/>
+          </div>
+            </Form>
+        </ModalBody>
+        <div className="d-flex justify-content-center mb-3">
+            <button type="submit" onClick={(event)=>handelconnectsubmit(event)} className="btn btn-success">Créer</button>
+        </div>
+        </Modal>
+      );
+    }else{
+      return(
+        <Modal isOpen={keysignup} toggle={toggles}>
+        <ModalHeader toggle={toggles}>Inscrivez vous</ModalHeader>
+        <ModalBody>
+            <Form>
+            <div class="mb-3 row">
+                  <label for="nom" className="col-sm-2 col-form-label">Nom complet</label>
+                  <div className="col-sm-10">
+                  <input type="text" onChange={handlchangename} readonly className="form-control-plaintext" id="nom"/>
+                  </div>
+           </div>
+           <div className="mb-3 row">
+           <label for="exampleDataList" className="col-2 form-label">Ville</label>
+              <input className="col-10 form-control" onChange={handlchangeville}list="datalistOptions" id="exampleDataList" placeholder="votre ville"/>
+              <datalist id="datalistOptions">
+              <option value="Tanger"/>
+              <option value="Tetouan"/>
+              <option value="Chefchaouen"/>
+              <option value="Hoceima"/>
+              <option value="Asila"/>
+              </datalist>
+           </div>
+            <div className="mb-3 row">
+                  <label for="staticEmail1" className="col-sm-2 col-form-label">Email</label>
+                  <div className="col-sm-10">
+                  <input type="text" readonly onChange={handlchangeemail} className="form-control-plaintext" id="staticEmail1" placeholder="Votre Email"/>
+                  </div>
+           </div>
+          <div className="mb-3 row">
+                  <label for="inputPassword" className="col-sm-2 col-form-label">Mot de passe</label>
+                  <div className="col-sm-10">
+                  <input type="password" onChange={handlchangepass} className="form-control" id="inputPassword"/>
+                  </div>
+          </div>
+            </Form>
+        </ModalBody>
+        <div className="d-flex justify-content-center mb-3">
+            <button type="submit" onClick={(event)=>handelconnectsubmit(event)} className="btn btn-success">Créer</button>
+        </div>
+        </Modal>
+      );
+    }
+  }
 return(
     
     <div>
@@ -78,13 +176,19 @@ return(
 
        <body>
            <div className="row">
-                <div className="col-md-6" style={{marginTop:"60px",fontFamily:"'Poppins', sans-serif"}} >  
+                <div className="col-md-6" style={{marginTop:"60px",fontFamily:"'EB Garamond', serif"}} >  
                  <div className="d-flex flex-column">
-                    <h4  className="p-2">Bienvenue dans votre site MinuteBrico</h4>
+                    <h4  className="mt-4 p-2">Bienvenue dans votre site MinuteBrico</h4>
                     <h5 className="p-2">Postez vos problémes ou prennez des opportunitées de bricolage</h5>
-                    <div className=" form" style={{marginTop:"150px"}} >
-                        <button id="connecter" className="btn" style={{background:"#383e56", color:"white",marginRight:"10px"}} onClick={()=>{setkeyc(!keyconnection)}} >se connecter</button>
-                        <button id="inscrire" className="btn btn-outline-dark" onClick={()=>setkeys(!keysignup)}>s'inscrire</button>
+                    <h4  className="p-2">مرحبا في موقعكم</h4>
+                    <h5 className="p-2">انشرو مشاكلكم حتى تجدو من يحلها</h5>
+                    <div className=" form" style={{marginTop:"110px"}} >
+                        <button id="connecter" className="btn" style={{height:"70px",width:"190px",borderRadius:"150px" ,background:"#383e56", color:"white",marginRight:"20px"}} onClick={()=>{setkeyc(!keyconnection)}} >se connecter</button>
+                        <button id="inscrire" style={{height:"70px",width:"200px",borderRadius:"100px"}} className="btn btn-outline-dark" onClick={()=>setkeys(!keysignup)}>s'inscrire</button>
+                    </div>
+                    <div className="d-flex justify-content-center form-check form-switch">
+                      <label className="mt-5 pb-2 form-check-label" style={{paddingRight:"65px"}} for="flexSwitchCheckDefault">Bricoleur ?</label>
+                      <input className="mt-5 pb-2 form-check-input" style={{height:"30px",width:"50px"}} onClick={()=>setBrico(!isBrico)} type="checkbox" id="flexSwitchCheckDefault"/>
                     </div>
                  </div>  
                 </div>
@@ -93,55 +197,8 @@ return(
                         <img src='assets/careerb.png' style={{width:"550px",height:"550px",/*boxShadow:"0px 4px 12px #c5d7bd"*/}}/>   
                 </div>
             </div> 
-            <Modal isOpen={keysignup} toggle={toggles}>
-              <ModalHeader toggle={toggles}>Inscrivez vous</ModalHeader>
-              <ModalBody>
-                  <Form>
-                  <div class="mb-3 row">
-                        <label for="nom" className="col-sm-2 col-form-label">Nom et Prénom</label>
-                        <div className="col-sm-10">
-                        <input type="text" onChange={handlchangename} readonly className="form-control-plaintext" id="nom"/>
-                        </div>
-                 </div>
-                 <div className="mb-3 row">
-                        <label for="domaine" className="col-sm-2 col-form-label">Domaine</label>
-                        <div className="col-sm-10">
-                        <input type="text" onChange={handlchangedomaine} readonly className="form-control-plaintext" id="domaine" placeholder="Votre domaine ou spécialité"/>
-                        </div>
-                 </div>
-                 <div className="mb-3 row">
-                 <label for="exampleDataList" className="form-label">Ville</label>
-                    <input className="form-control" onChange={handlchangeville}list="datalistOptions" id="exampleDataList" placeholder="votre ville"/>
-                    <datalist id="datalistOptions">
-                    <option value="Tanger"/>
-                    <option value="Tetouan"/>
-                    <option value="Chefchaouen"/>
-                    <option value="Hoceima"/>
-                    <option value="Asila"/>
-                    </datalist>
-                 </div>
-                  <div className="mb-3 row">
-                        <label for="staticEmail1" className="col-sm-2 col-form-label">Email</label>
-                        <div className="col-sm-10">
-                        <input type="text" readonly onChange={handlchangeemail} className="form-control-plaintext" id="staticEmail1" placeholder="Votre Email"/>
-                        </div>
-                 </div>
-                <div className="mb-3 row">
-                        <label for="inputPassword" className="col-sm-2 col-form-label">Mot de passe</label>
-                        <div className="col-sm-10">
-                        <input type="password" onChange={handlchangepass} className="form-control" id="inputPassword"/>
-                        </div>
-                </div>
-                <div className="mb-3">
-                <label for="formFileMultiple" className="form-label">Photo et CV</label>
-                <input className="form-control" type="file" id="formFileMultiple" multiple/>
-                </div>
-                  </Form>
-              </ModalBody>
-              <ModalFooter>
-                  <button type="submit" onClick={(event)=>handelconnectsubmit(event)} className="btn btn-primary">Créer</button>
-              </ModalFooter>
-            </Modal>
+          
+          {inscrire(isBrico,keysignup,toggles)}
 
 
             <Modal isOpen={keyconnection} toggle={toggles}>
@@ -162,9 +219,9 @@ return(
                 </div>
                   </Form>
               </ModalBody>
-              <ModalFooter>
-                  <button type="submit" onClick={(event)=>handlsignupsubmoit(event)} className="btn btn-primary">Créer</button>
-              </ModalFooter>
+              <div className="d-flex justify-content-center mb-3">
+                 <button type="submit" onClick={(event)=>handlsignupsubmoit(event)} className=" btn btn-success ">Connecter</button>
+              </div>
             </Modal>
       </body>
 
