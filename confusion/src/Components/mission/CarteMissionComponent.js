58f77b79ac09da42799199c63d41e53e9e5e52d7
imad barrahou <imadbarrahou99@gmail.com>
2021-04-20 02:39:36 +0000

    adding pagination

--- confusion/src/Components/mission/CarteMissionComponent.js
+++ confusion/src/Components/mission/CarteMissionComponent.js
@@ -7,30 +7,30 @@ const MissionItems= (props)=>{
        return(
      
            <div  key={item.id} className="container mt-2" >
-              <div className="card  text-white" style={{color:"white" , borderRadius:"25px",border:"solid" }}>  
+              <div className="card  text-white" style={{color:"white" , borderRadius:"5px",border:"solid" }}>  
          
               <div className="card-img ">
                     <div id="carouselExampleSlidesOnly"  className="carousel slide " data-bs-ride="carousel">
                         <div className="carousel-inner " >
                                 <div className="carousel-item active">
-                                <img id="missionImage" style={{height:"200px",width:"850px", borderRadius:"25px"}} src={item.image}/>
+                                <img id="missionImage" style={{height:"200px",width:"850px", borderRadius:"5px"}} src={item.image}/>
                                 </div>
                                 <div className="carousel-item">
-                                <img style={{height:"200px",width:"850px", borderRadius:"25px"}} src={item.image}/>
+                                <img style={{height:"200px",width:"850px", borderRadius:"5px"}} src={item.image}/>
                                 </div>
                                 <div className="carousel-item">
-                                <img style={{height:"200px",width:"850px", borderRadius:"25px"}} src={item.image}/>
+                                <img style={{height:"200px",width:"850px", borderRadius:"5px"}} src={item.image}/>
                                 </div>
                        </div>
                  </div>
                  </div>
                         <div className="card-img-overlay">
-                            <div className="d-flex flex-column" style={{border:"1px solid #91091E",opacity: "0.5",width:"350px" , height:"150px",background:"#91091E"}}>
-                                 <h3 className="card-title " style={{color:"#D1653E",fontFamily:"'Lato', sans-serif",position:"relative",left:"-100px"}}>{item.titre_mission}</h3>
-                                 <p className="card-text" style={{fontFamily:"'Lato', sans-serif",marginRight:"180px"}}>{item.description}</p>
+                            <div className="d-flex flex-column" style={{border:"1px  #91091E",opacity: "0.5",width:"350px" , height:"150px",background:"gray"}}>
+                                 <h3 className="card-title " style={{color:"#D1653E",fontFamily:"'Lato', sans-serif",position:"relative",left:"-110px"}}>{item.titre_mission}</h3>
+                                 <p className="card-text" style={{fontFamily:"'Lato', sans-serif",marginRight:"120px"}}>{item.description}</p>
                                <div className="d-flex flex-row">
                                {item.categories.map((x)=>{
-                                  return(<p className="p-1" key={x.id}>{ x.nom} </p>); 
+                                  return(<p className="p-1 fw-light bg-success" key={x.id}>{ x.nom} </p>); 
                                  })}
                                </div>
                                  
@@ -51,3 +51,7 @@ const MissionItems= (props)=>{
 
 }
  export default MissionItems;
+
+
+
+
--- confusion/src/Components/mission/MissionsComponent.js
+++ confusion/src/Components/mission/MissionsComponent.js
@@ -3,12 +3,20 @@ import useSWR from 'swr'
 import Nav from '../home/nav';
 import MissionItems from './CarteMissionComponent';
 import './mission.css'
+import Footer from '../home/Footer';
+import Pagination from './PaginationComponent';
 const Mission = ()=>{
   const[categorie,setCategorie]=React.useState('Tout');
   const[radioBoutton,setradioBoutton]=React.useState(2);
  const [categories,setCategories]=React.useState([]);
  const [donnee,setdonnee]=React.useState(["Menuiserie"]);
+const [motcle,setMotcle]=React.useState(' ');
+const[currentpage,setCurrentpage]=React.useState(1);
+const[postperpage,setPostperpage]=React.useState(4);
 
+const indexOfLastCarte=currentpage*postperpage;
+const indexOfFirstCarte=indexOfLastCarte - postperpage;
+const paginate= pageNumber=>setCurrentpage(pageNumber);
   function radioHandler(event){
 
       setradioBoutton(1);
@@ -51,15 +59,25 @@ const Mission = ()=>{
     console.log("categories : focus "+categories);
  
   }
-
+    function handelmotcle(){
+      setMotcle(document.getElementById("motcle").value);
+    }
 
 
 const fetcher = (url) => fetch(url).then(res =>{return res.json()} )
  const { data, error } = useSWR('http://localhost:8080/missions', fetcher)
-    if (error) return <div>failed to load</div>
+    if (error) return (
+            <div className="d-flex flex-column">
+              <img alt="..." src="/assets/logo.png" className="logo" />
+
+                <p className="text-danger">failed to load</p>
+
+            </div>);
     if (!data) {return (
-    <div style={{position:"absolute",left:"45%",top:"45%" }}>      
-                <div className="spinner-grow text-primary" role="status">
+      <div>
+        <img alt="..." src="/assets/logo.png" className="logo" />
+        <div className="d-flex flex-row" style={{position:"absolute",left:"50%",right:"50%",top:"45%" }}>      
+           <div className="spinner-grow text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
             </div>
             <div className="spinner-grow text-secondary" role="status">
@@ -83,28 +101,39 @@ const fetcher = (url) => fetch(url).then(res =>{return res.json()} )
             <div className="spinner-grow text-dark" role="status">
             <span className="visually-hidden">Loading...</span>
             </div>  
+  </div>
   </div>);}
 
 
 //alert(data);
-console.log("categories empty ?"+(categories.length==0)+" les donnéesss  "+((data[0].categories.filter((y)=>(donnee.includes("hh"))).length==1)?true:false)+"  second   "+data.filter((x)=>((x.categories.filter((y)=>(categories.includes(y.nom))).length==0)?false:true)));
+const currentCartes=data.slice(indexOfFirstCarte,indexOfLastCarte);
+//setNombrecarte(data.length);
+
+
+console.log("nombre de cartes   : "+data.length+" categories empty ?"+(categories.length==0)+" les donnéesss  "+((data[0].categories.filter((y)=>(donnee.includes("hh"))).length==1)?true:false)+"  second   "+data.filter((x)=>((x.categories.filter((y)=>(categories.includes(y.nom))).length==0)?false:true)));
     return(
    <div  id="mission"  className="fluid-container" className="d-flex flex-column">
-       <div id="briconav" style={{position:"absolute",top:"0px"}}>
+       <div id="briconav" style={{position:"absolute",top:"0px",zIndex:"1"}}>
        <Nav/>
        </div>
-       <div id="bricotext" style={{border:"1px",textDecorationColor:"black",height:"100px",width:"100%",background:"#9BB9B9",position:"absolute",top:"120px"}}>
-        <h1 id="something" >Bricolage à la minute !</h1>
+       <div id="bricotext" className="d-flex justify-content-center flex-row" style={{border:"1px",textDecorationColor:"black",height:"100px",width:"100%",background:"#FFFFFF",position:"absolute",top:"160px",alignItems:"center"}}>
+       
+            <input type="text" className="form-control" style={{width:"30%",height:"40%",marginRight:"90px"}} placeholder="Rechercher pour des mots clés" id="motcle"/>
+            <button onClick={handelmotcle} style={{border:"1px ", background:"#D1653E",height:"40%",width:"10%",borderRadius:"5px",color:"white"}} >Rechercher</button>
+         
        </div>
-       <div className="container" style={{position:"relative",top:"220px"}} >
+       <div className="container" style={{position:"relative",top:"270px"}} >
        
 
 
            <div id="miss" className="container d-flex flex-row mt-3" style={{position:"relative",top:"0px"}}>
-                  <div className="col-md-4 mt-3" style={{border:"1px ",borderRadius:"5px",boxShadow:"3px 5px 25px",height:"450px",background:"white"}}>
-                  <h3>Filtrer par :</h3>
-                  <hr/>
-                  <h4>Catégories</h4>
+                  <div className="col-md-4 mt-3" style={{border:"1px ",borderRadius:"5px",height:"650px",background:"white"}}>
+                  <p style={{float:"left"}} className="fs-3 px-4">Filtrer par :</p>
+                <div className="container">
+                <hr style={{clear:"both"}}/>
+                </div>
+                
+                  <p style={{float:"left"}} className="fs-4 px-4">Catégories</p>
                   <div className="container d-flex flex-column">
                             <div className="form-check d-flex flex-row">
                                   <input className="form-check-input"   onClick={(event)=>handelcheckboxclick(event,categories)} type="checkbox"   value="Menuiserie" id="flexCheckDefault"/>
@@ -158,16 +187,31 @@ console.log("categories empty ?"+(categories.length==0)+" les donnéesss  "+((da
                                 
                                  
                             </div>
+                          
+                    
+
                             </div>
                       </div>
-                  </div>
-                  <div id="box" className="col-md-7" style={{ borderRadius:"2px",height:"450px",position:"absolute",right:"70px",top:"0px"}} > 
-                      
-                     <MissionItems data={data.filter((x)=>(((x.categories.filter((y)=>(categories.includes(y.nom))).length==0)?false:true)||(categories.length==0)))} />
+                
+                   <div className="col-md-7"  style={{background:"white",position:"relative",top:"14px",left:"30px",height:"650px"}}>
+                       <div style={{position:"relative",top:"10px",left:"45%"}}>
+                      <Pagination totalcartes={data.length} carteparpage={postperpage} paginate={paginate} />
 
+                      </div>
+
+                      <div className="container">
+                          <hr />
+                      </div>
+
+                      <div id="box"  style={{ borderRadius:"2px",height:"90%",width:"96%",position:"realative",top:"40px"}} > 
+                          
+                        <MissionItems data={currentCartes.filter((x)=>((((x.categories.filter((y)=>(categories.includes(y.nom))).length==0)?false:true) && (x.description.includes(motcle)) )||(((x.categories.filter((y)=>(categories.includes(y.nom))).length==0)?false:true) && (motcle==' ') )||((categories.length==0)  && (x.description.includes(motcle)))||((categories.length==0)  && (motcle==' '))))} />
+                      
+                      </div>
                   </div>
-     
+                  </div>
           </div>
+        
         </div>
 
     );
