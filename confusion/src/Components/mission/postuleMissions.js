457b250e57047965881389b75fe63c2f0701472d
imad barrahou <imadbarrahou99@gmail.com>
2021-06-02 14:30:50 +0100

    solution for postule liste

--- confusion/src/Components/mission/postuleMissions.js
+++ confusion/src/Components/mission/postuleMissions.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React ,{useEffect, useState} from 'react';
 import useSWR from 'swr'
 import Axios from "axios"
 import Nav from '../home/nav';
@@ -9,7 +9,10 @@ import '../home/Cards.css';
 
 const PostuleMission=(props)=>{
   const [show,setShow]=useState(false);
+  const[ Array,setArray]=([]);
+  const [Donnee,setDonnee]=useState(<div  style={{color:"black",fontSize:"13em"}} >empty</div>);
   const [acceper,setEstaccepe]=useState(0);
+  const [Div,setDiv]=useState(<div  style={{color:"black",fontSize:"13em"}} > div</div>)
   const handelAccept =(mission)=>{
     //alert("http://localhost:8080/BricoAccept/"+props.user.id+"/"+mission+"/1")
     Axios.put("http://localhost:8080/BricoAccept/"+props.user.id+"/"+mission+"/1")
@@ -22,86 +25,102 @@ const PostuleMission=(props)=>{
           })
   }  
 
-const fetcher = (url) => fetch(url).then(res =>{return res.json()} )
-const { data, error } = useSWR('http://localhost:8080/bricoleurs/'+props.user.id, fetcher)
-   if (error) return (
-           <div className="d-flex flex-column">
-             <img alt="..." src="/assets/logo.png" style={{height:"100px",width:"100px"}} className="logo" />
+useEffect(()=>{
+ 
+  Axios.get('http://localhost:8080/bricoleurs/'+props.user.id)
+  .then(res=>{
+    console.log({darais:res.data})
+    setDonnee(res.data.missions.map( (item) =>{
 
-               <p className="text-danger">failed to load</p>
+       return item.accepterBrico.map(i=>{
 
-           </div>);
-   if (!data) {return (
-     <div>
-       <img alt="..." src="/assets/logo.png" className="logo" />
-       <div className="d-flex flex-row" style={{position:"absolute",left:"50%",right:"50%",top:"45%" }}>      
-          <div className="spinner-grow text-primary" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-secondary" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-success" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-danger" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-warning" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-info" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-light" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>
-           <div className="spinner-grow text-dark" role="status">
-           <span className="visually-hidden">Loading...</span>
-           </div>  
- </div>
- </div>);}
-
-
-//les cartes  
-
-const Missions= data.missions.map((item)=>{
-    console.log("hello"+ item.id);
+        if(i.bricoId==props.user.id){
           
-    Axios.get("http://localhost:8080/accepterbrico/"+props.user.id+"/"+item.id)
-          
-    .then(res=>{
-      setEstaccepe(res.data)
-     // alert("accepter response "+ res.data)
-    })
-    .catch(err =>{
-    alert(err)
-        })
-
-
-    const id=item.id;
-       return(
-        <div className='cards'>
-        <div className='cards__container'>
-     
-           
-              <CardItem
-                key={item.id}
-                src={item.images}
-                text={item.mission_description}
-                label={item.titre_mission}
-                path='/comment'
-              />
-              {(acceper==2)?  <div className="d-flex flex-row align-items-around p-3">
-                  <button onClick={()=>handelAccept(item.id)} className="btn btn-outline-success">Accepter</button>
-                  <button className="btn btn-outline-danger">Refuser</button>
-              </div>:((acceper==1)?<div style={{color:"green",fontSize:"2em"}} ><p>Vous l'avais accepté</p></div>:<div style={{color:"red",fontSize:"2em"}} >Pas de réponse</div>)}
+         
+          if(i.clientAccept==1 && i.bricoAccept==1 ){
+          //  setEstaccepe(1) ;
+            console.log({estAccept:1})
+       
+  
+           return (
+            <div>
+              <CardItem  key={i.id}
+                  src={item.images}
+                  text={item.mission_description}
+                  label={item.titre_mission}
+                  path='/comment'
+                />
             
-      </div>
-    </div>
-       )
-    });
+              <div style={{color:"green",fontSize:"2em"}} ><p>Vous l'avais accepté</p></div>
+          </div>
+                
+            
+            
+            
+            ); 
+          }
+  
+          else if( i.clientAccept==1 && i.bricoAccept==0 ){
 
+            console.log({estAccept:2})
+            setEstaccepe(2) ;
+            return (
+              <div>
+                <CardItem    key={i.id}
+                  src={item.images}
+                  text={item.mission_description}
+                  label={item.titre_mission}
+                  path='/comment'
+                      />
+                 <div className="d-flex flex-row align-items-around p-3">
+                <button onClick={()=>handelAccept(item.id)} className="btn btn-outline-success">Accepter</button>
+                <button className="btn btn-outline-danger">Refuser</button>
+            </div>
+              </div>
+            );
+          }
+          else{
+        
+              console.log({estAccept:0})
+                return(
+                          <div>
+                          <CardItem  key={i.id}
+                              src={item.images}
+                              text={item.mission_description}
+                              label={item.titre_mission}
+                              path='/comment'
+                              />
+                        <div style={{color:"red",fontSize:"2em"}} >Pas de réponse</div>
+                        </div>
+
+
+                        );
+      
+         
+          }
+  
+         } else{
+           return(
+             <div   style={{color:"black",fontSize:"13em"}}>
+               vide
+             </div>
+           );
+         }
+
+
+      })
+
+     
+     
+    })
+  )
+
+  })
+  .catch(err => console.error(err))
+
+},[])
+
+console.log({Donne:Donnee})
     return(
     <div >
       <div id="postulemission"></div>
@@ -117,7 +136,10 @@ const Missions= data.missions.map((item)=>{
                 setShow(false)
               }}
             />
-             {Missions}
+            <div className="container-fluid" style={{width:"40%"}}>
+            {Donnee}
+            </div>
+             
            </div>
                    
     </div>
