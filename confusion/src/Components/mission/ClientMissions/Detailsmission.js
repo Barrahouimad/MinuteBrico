9eb78a06f2c1c3b13963ed17e7aaccd50686885e
kayachi36 <elayachikaoutar'@gmail.com>
2021-06-06 11:41:35 +0100

    Détails missions vue client

--- confusion/src/Components/mission/ClientMissions/Detailsmission.js
+++ confusion/src/Components/mission/ClientMissions/Detailsmission.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import Nav from '../../home/nav';
 import './ClientMissionCss.css'
-import Carte from './carte'
+import Carte from './Carte'
 import CarteBrico from './carteBrico'
-const DetailsMissions=(props)=>{
+const DetailsMissions = (props) => {
 
 
-    return(
+    return (
         <div id="detailsMissions">
-           <Nav data={props.data}/>
-           <div  className=" mt-3">
+            <Nav data={props.data} />
+            <div className=" mt-3">
                 <div className="row justify-content-center">
-                    <div style={{background:"#D1653E",textColor:"white",padding:"5px",paddingRight:"25em"}} className="col-md-6 col-xs-10">
-                            <h1>Récapitulatif de vos missions</h1>
+                    <div className="col-md-6 col-xs-10">
+                        <h6>Récapitulatif de vos missions postées: </h6>
                     </div>
-                    <div style={{background:"#D1653E",marginLeft:"10px",padding:"5px"}} className="ml-3 col-md-2 col-xs-10">
-                            <h1>Status de mission</h1>
+                    <div className=" col-md-2 col-xs-10">
+                        <h6>Status:</h6>
+                    </div>
+                </div>
+                <div className=" mt-3">
+                    <Carte data={((props.data.length != 0) ? (props.data.missions.filter((x) => (x.id == document.location.pathname.split("/")[2]))) : (props.data))} />
+                </div>
+                <br />
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-xs-10">
+                        <h6>Les MinuteBricoleurs ayant postulé à votre mission:</h6>
+                    </div>
+                    <div className=" col-md-2 col-xs-10">
+                    </div>
+                </div>
 
-                    </div>
-               </div>
-                <div className=" mt-4">
-                    <Carte data={( (props.data.length!=0)?  (props.data.missions.filter((x)=>(x.id==document.location.pathname.split("/")[2]))):(props.data)  )}/>
-               </div>
-               <div style={{marginLeft:"250px"}} className="row justify-content-start mt-3">
-                    <div style={{background:"#D1653E",textColor:"white",padding:"5px",marginTop:"20px",marginLeft:"5em",paddingRight:"18em"}} className="col-md-6 col-xs-10">
-                            <h1>Liste bricoleurs ayant postulés</h1>
-                    </div>
-                    
-               </div>
-
-               <div className="container">
-                   <CarteBrico data={( (props.data.length!=0)?  (props.data.missions.filter((x)=>(x.id==document.location.pathname.split("/")[2]))):(props.data)  )} missionId={document.location.pathname.split("/")[2]}/>
-               </div>
-           </div>
+                <div className="container">
+                    <CarteBrico data={((props.data.length != 0) ? (props.data.missions.filter((x) => (x.id == document.location.pathname.split("/")[2]))) : (props.data))} missionId={document.location.pathname.split("/")[2]} />
+                </div>
+            </div>
         </div>);
 
 }
--- confusion/src/Components/mission/ClientMissions/carteBrico.js
+++ confusion/src/Components/mission/ClientMissions/carteBrico.js
@@ -1,74 +1,74 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Axios from "axios"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import SweetAlert from 'sweetalert2-react';
-const CarteBrico =(props)=>{
-      const [brico,setBrico]=useState([]);
-      const [show,setShow]=useState(false);
-     
-      const handelAccept =(brico)=>{
-       // alert("http://localhost:8080/ClientAccept/"+brico+"/"+props.missionId+"/1")
-        Axios.put("http://localhost:8080/ClientAccept/"+brico+"/"+props.missionId+"/1")
-     
-          .then(res=>{
-            setShow(true)
-          })
-          .catch(err =>{
-           alert(err)
-              })
-      }
-React.useEffect(()=>{
-  Axios.get( "http://localhost:8080/Bricoofmissions/"+props.missionId)
+const CarteBrico = (props) => {
+  const [brico, setBrico] = useState([]);
+  const [show, setShow] = useState(false);
 
-  .then(res=>{
-       setBrico(res.data);
-      
-   })  
-   .catch(err =>{
-     console.log("still have error ",err);
-   })
-},[])
-   
-    
-const carte=brico.map((brico)=>{
-  return(
-     <div key={brico.id} className="card mt-3 d-flex flex-row " style={{width:"65em",marginLeft:"8em"}} >
-         <div style={{width:"80%"}} className=" row justify-content-between">
-             <div className="col-md-3  p-3">
-                <img src="/assets/logo.png" style={{height:"110px",width:"70px"}}/>
-             </div>
-             <div className="col-md-auto  p-3">
-                 <a className="navlink" href={"http://localhost:3000/profile/"+brico.id}> <p style={{color:"black"}} > {brico.firstName+" "+brico.lastName}</p></a>
-             </div>
-             <div className="col-md-3 p-3">
-                 <h3>Disponibilité </h3>
-             </div>
-         </div>
-         <div className="d-flex flex-column align-items-around p-3">
-              <button onClick={()=>handelAccept(brico.id)} className="btn btn-outline-success">Accepter</button>
-              <button className="btn btn-outline-danger">Refuser</button>
+  const handelAccept = (brico) => {
+    // alert("http://localhost:8080/ClientAccept/"+brico+"/"+props.missionId+"/1")
+    Axios.put("http://localhost:8080/ClientAccept/" + brico + "/" + props.missionId + "/1")
 
-         </div>
-         <SweetAlert
-              show={show}
-              title="Success"
-              icon='warning'
-              text="Merci ! On attend maintenant la réponse du bricoleur"
-              onConfirm={() => {
-                setShow(false)
-              }}
-            />
-    </div>)
-});
+      .then(res => {
+        setShow(true)
+      })
+      .catch(err => {
+        alert(err)
+      })
+  }
+  React.useEffect(() => {
+    Axios.get("http://localhost:8080/Bricoofmissions/" + props.missionId)
 
-    return(
-     <div>
-        {(props.data.length!=0)?((props.data[0].etat_mission==0)?<div>
+      .then(res => {
+        setBrico(res.data);
+
+      })
+      .catch(err => {
+        console.log("still have error ", err);
+      })
+  }, [])
+
+
+  const carte = brico.map((brico) => {
+    return (
+      <div key={brico.id} className="card mt-3 d-flex flex-row " style={{ width: "65em", marginLeft: "8em" }} >
+        <div style={{ width: "80%" }} className=" row justify-content-between">
+          <div className="col-md-3  p-3">
+            <img src="/assets/logo.png" style={{ height: "110px", width: "70px" }} />
+          </div>
+          <div className="col-md-auto  p-3">
+            <a className="navlink" href={"http://localhost:3000/profile/" + brico.id}> <p style={{ color: "black" }} > {brico.firstName + " " + brico.lastName}</p></a>
+          </div>
+          <div className="col-md-3 p-3">
+            <h3>Disponibilité </h3>
+          </div>
+        </div>
+        <div className="d-flex flex-column align-items-around p-3">
+          <button onClick={() => handelAccept(brico.id)} className="btn btn-outline-success">Accepter</button>
+          <button className="btn btn-outline-danger">Refuser</button>
+
+        </div>
+        <SweetAlert
+          show={show}
+          title="Success"
+          icon='warning'
+          text="On vous remercie, prière d'attendre la réponse de votre Bricoleur."
+          onConfirm={() => {
+            setShow(false)
+          }}
+        />
+      </div>)
+  });
+
+  return (
+    <div>
+      {(props.data.length != 0) ? ((props.data[0].etat_mission == 0) ? <div>
         {carte}
-        </div>:
-       <div>
-       <p style={{color:"gray" , marginRight:"20em",marginTop:"1em",fontSize:"2em"}} >Un bricoleur est choisi</p></div>):<div></div>}
-     </div>
-        );
+      </div> :
+        <div>
+          <p style={{ color: "gray", marginRight: "20em", marginTop: "1em", fontSize: "2em" }} >Un bricoleur est choisi</p></div>) : <div></div>}
+    </div>
+  );
 }
 export default CarteBrico;
